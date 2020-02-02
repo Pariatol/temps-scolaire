f512a762279c0a234bc253339b343ec4ecbc70d4
Pariatol <pierre.tromeur@gmail.com>
2020-02-02 21:31:09 +0100

    Updates

---
+++ precache-manifest.c28d85b9fc614a5a8600b06aa492ba0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb2302abb1f5790f1b7a2c4e3b28740",
+    "url": "/temps-scolaire/index.html"
+  },
+  {
+    "revision": "5cd22fb8baa8eb1fbe99",
+    "url": "/temps-scolaire/static/css/main.4edc6486.chunk.css"
+  },
+  {
+    "revision": "adb89d97434326e7ed67",
+    "url": "/temps-scolaire/static/js/2.09f7555f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/temps-scolaire/static/js/2.09f7555f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cd22fb8baa8eb1fbe99",
+    "url": "/temps-scolaire/static/js/main.ae737ba4.chunk.js"
+  },
+  {
+    "revision": "cffcd9a414af5a3d9fde",
+    "url": "/temps-scolaire/static/js/runtime-main.c0984c2e.js"
+  },
+  {
+    "revision": "bd28b42fd610fefca03fb2715b533315",
+    "url": "/temps-scolaire/static/media/school.bd28b42f.jpg"
+  }
+]);
